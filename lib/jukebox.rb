5c813ed1742ec232853f66741b045c667228d4fa
Jacob Kagon <jacob@kagon.net>
2020-09-23 23:30:31 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -44,3 +44,20 @@ def exit_jukebox
   puts "Goodbye"
 end 
 
+def run(songs)
+  puts "Please enter a command:"
+  loop do
+    input = gets.chomp
+    case input
+    when "help"
+      help
+    when "list"
+      list(songs)
+    when "play"
+      play(songs)
+    when "exit"
+      exit_jukebox
+      break
+    end
+  end
+end
