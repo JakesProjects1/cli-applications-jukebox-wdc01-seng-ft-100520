c267df1506f885b396c7841b9843ce9d5555600d
Jacob Kagon <jacob@kagon.net>
2020-09-23 23:29:58 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -27,7 +27,17 @@ def list(songs)
   end
 end
 
-
+def play(songs)
+  puts "Please enter a song name or number:"
+  input = gets.chomp
+  if ((1..songs.size).include? input.to_i)
+    puts "Playing #{songs[input.to_i - 1]}"
+  elsif songs.include?(input)
+    puts "Playing #{input}"
+  else
+    puts "Invalid input, please try again"
+  end
+end
 
 
 def exit_jukebox
