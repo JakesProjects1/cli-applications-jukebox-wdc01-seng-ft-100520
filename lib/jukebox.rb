f5b9ea339984631564ed68597b9bfa1076b4cb61
Jacob Kagon <jacob@kagon.net>
2020-09-23 23:22:09 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -45,25 +45,3 @@ def exit_jukebox
 end 
 
 
-def run(songs)
-  help 
-  input = "" 
-  while input != "exit"  
-  puts "Please enter a command:"
-  input = gets.chomp
-  case input 
-  when "play"
-    play(songs)
-  when "list"
-    list(songs)
-  when "help"
-    help
-  when "exit"
-    exit_jukebox
-  else
-    puts "Invalid input, please try again"
-  end
-end
-end
-    
-  
